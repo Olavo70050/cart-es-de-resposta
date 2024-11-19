1c2eba7a55b67f55dfe48a9a34f5b29480c8ddea
Olavo70050 <00001082916055sp@al.educacao.sp.gov.br>
2024-11-19 15:44:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Peso',
+    'quanto pesa uma nuvem?',
+    'A média do peso é de 500 mil quilos podendo variar se for uma nuvem com muita água'
+)
+
+criaCartao(
+    'Água',
+    'Quantos litros de água tem o oceano?',
+    'A estimativa é que há 1,332 bilhões de quilômetros cúbicos de água nos oceanos'
+)
+
+criaCartao(
+    'Bioma',
+    'Qual é o maoir deserto do mundo?',
+    'Não é o saara mas sim a antártica.Desertos nem sempre são quentes.'
+)
+
+criaCartao(
+    'Universo',
+    'Quanto tempo dura um dia em Vênus?',
+    'um dia em Vênus dura 243 dias.'
+)
